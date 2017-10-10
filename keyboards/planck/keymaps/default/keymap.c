e7f6701990bb66f520ae26b0bc8fc5f5aba89226
Jason Stillwell <dragonfax@gmail.com>
2017-10-10 13:05:22 -0700

    re-enable hand swapping

--- keyboards/planck/keymaps/default/keymap.c
+++ keyboards/planck/keymaps/default/keymap.c
@@ -39,7 +39,6 @@ enum planck_keycodes {
   BACKLIT,
   EXT_PLV,
   PASSWRD,
-  SWAPHND = ACTION_SWAP_HANDS_TAP_KEY(KC_SPC),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -59,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_GESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT },
-  {_______, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   SWAPHND,  SWAPHND,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {_______, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_FN0,  KC_FN0,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Colemak
@@ -178,6 +177,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   float plover_song[][2]     = SONG(PLOVER_SOUND);
   float plover_gb_song[][2]  = SONG(PLOVER_GOODBYE_SOUND);
 #endif
+
+const uint16_t PROGMEM fn_actions[] = {
+  [0] = ACTION_SWAP_HANDS_TAP_KEY(KC_SPC),
+};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
