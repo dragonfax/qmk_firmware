7c3b59ab5e5e9baed4248b1238b229d140b8bcff
Jason Stillwell <dragonfax@gmail.com>
2017-10-10 12:51:55 -0700

    Revert "Revert "try to enable swap hands""
    This reverts commit b965e3e92954d8718de787c3f74a856927e7ad45.

--- keyboards/planck/keymaps/default/keymap.c
+++ keyboards/planck/keymaps/default/keymap.c
@@ -38,7 +38,8 @@ enum planck_keycodes {
   RAISE,
   BACKLIT,
   EXT_PLV,
-  PASSWRD
+  PASSWRD,
+  SWAPHND = ACTION_SWAP_HANDS_TAP_KEY(KC_SPC),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -58,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_GESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT },
-  {_______, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {_______, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   SWAPHND,  SWAPHND,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Colemak
