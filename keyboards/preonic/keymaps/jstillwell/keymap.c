f62685f7327699b5d81449a92c59cdb152efb8a1
Jason Stillwell <dragonfax@gmail.com>
2018-03-12 00:14:27 -0700

    restoring reset command and extra layer for it

--- keyboards/preonic/keymaps/jstillwell/keymap.c
+++ keyboards/preonic/keymaps/jstillwell/keymap.c
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD(TD_BRACKETS)},
   {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_QUOT) },
-  {KC_LCTL, KC_NO,   KC_LGUI, KC_LALT, MO(_LOWER),   KC_FN0,  KC_FN0,  MO(_LOWER),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_LCTL, KC_NO,   KC_LGUI, KC_LALT, LOWER,   KC_FN0,  KC_FN0,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Lower
@@ -103,23 +103,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, _______, _______, _______, _______, KC_BSPC, KC_BSPC, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END}
 },
 
+[_ADJUST] = {
+  {_______, _______, _______, _______, _______, RESET, _______, _______, _______, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+},
+
 };
+
+bool raise_pressed = false;
+bool lower_pressed = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
         case LOWER:
+          lower_pressed = record->event.pressed;
           if (record->event.pressed) {
             layer_on(_LOWER);
+            if (lower_pressed && raise_pressed) {
+              layer_on(_ADJUST);
+            }
           } else {
             layer_off(_LOWER);
+            layer_off(_ADJUST);
           }
           return false;
           break;
         case RAISE:
+          raise_pressed = record->event.pressed;
           if (record->event.pressed) {
             layer_on(_LOWER);
+            if (lower_pressed && raise_pressed) {
+              layer_on(_ADJUST);
+            }
           } else {
             layer_off(_LOWER);
+            layer_off(_ADJUST);
           }
           return false;
           break;
