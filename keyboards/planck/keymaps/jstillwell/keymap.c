be99dbbf3858e14b680b8c8edae940210e2778ba
Jason Stillwell <dragonfax@gmail.com>
2018-03-15 00:31:42 -0700

    remove tap dance

--- keyboards/planck/keymaps/jstillwell/keymap.c
+++ keyboards/planck/keymaps/jstillwell/keymap.c
@@ -32,34 +32,6 @@ enum planck_keycodes {
   RAISE,
 };
 
-enum {
-  TD_BRACKETS = 0,
-  TD_DASHES,
-};
-
-void dance_brackets (qk_tap_dance_state_t *state, void *user_data) {
-  if (state->count == 1) {
-    register_code(KC_LBRC);
-    unregister_code(KC_LBRC);
-    reset_tap_dance (state);
-  } else if (state->count == 2) {
-    register_code(KC_RBRC);
-    unregister_code(KC_RBRC);
-    reset_tap_dance (state);
-  } else {
-    register_code(KC_BSLASH);
-    unregister_code(KC_BSLASH);
-    reset_tap_dance (state);
-  }
-}
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-  // Tap once for [, twice for ], 3 times for \ (back slash)
-  [TD_BRACKETS] = ACTION_TAP_DANCE_FN(dance_brackets),
-  [TD_DASHES] = ACTION_TAP_DANCE_DOUBLE(KC_MINUS,KC_EQUAL),
-};
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* http://www.keyboard-layout-editor.com/#/gists/e0f05dcdbec66445c2f4449b9298b2b1 */
@@ -76,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD(TD_BRACKETS)},
+  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______},
   {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT},
   {OSM(MOD_LSFT), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_QUOT) },
   {KC_LCTL, KC_NO,   KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
@@ -86,16 +58,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |  `   |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | - =  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |  Del |  Del |  Del |  Del |  Del |  Del |   Del|   Del|   Del|   Del|      |
+ * |      |  Del |  Del |  Del |  Del |      |      |   -  |   =  |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |             |   [  |   ]  |   \  |  '   |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | HOME | PgDn | PgUp | END  |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
-  {KC_GRAVE,KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    TD(TD_DASHES)},
-  {_______, KC_BSPC, KC_BSPC, KC_BSPC, KC_BSPC, KC_BSPC, KC_BSPC, KC_BSPC, KC_BSPC, KC_BSPC, KC_BSPC, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+  {KC_GRAVE,KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______},
+  {_______, KC_BSPC, KC_BSPC, KC_BSPC, _______, _______, _______, KC_MINUS,KC_EQUAL,_______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC,KC_BSLASH,KC_QUOT, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END}
 },
 
--- keyboards/planck/keymaps/jstillwell/rules.mk
+++ keyboards/planck/keymaps/jstillwell/rules.mk
@@ -1,6 +1,6 @@
 
 NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-TAP_DANCE_ENABLE = yes
+TAP_DANCE_ENABLE = no
 ONEHAND_ENABLE = no
 EXTRAKEY_ENABLE = no       # Audio control and System control(+450)
 CONSOLE_ENABLE = no         # Console for debug(+400)
