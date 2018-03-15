9667ac768a92cf2ba4c9e358f9b917e896a3ea62
Jason Stillwell <dragonfax@gmail.com>
2018-03-15 01:26:52 -0700

    DRY number row

--- keyboards/planck/keymaps/jstillwell/keymap.c
+++ keyboards/planck/keymaps/jstillwell/keymap.c
@@ -34,6 +34,7 @@ enum planck_keycodes {
 };
 
 #define KC_VOLDN KC__VOLDOWN
+#define NUM_ROW {KC_GRAVE,KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  _______ }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -52,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = {
 #if MATRIX_ROWS == 5
-  {KC_GRAVE,KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______},
+  NUM_ROW ,
 #endif
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______},
   {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT },
@@ -76,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
 #else
-  {KC_GRAVE,KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______},
+  NUM_ROW ,
 #endif
   {_______, DELETE,  DELETE,  DELETE,  DELETE,  _______, _______, KC_MINUS,KC_EQUAL,_______, _______, _______},
   {_______, KC__MUTE,KC__VOLUP,KC_VOLDN,_______,_______, _______, KC_LBRC, KC_RBRC,KC_BSLASH,_______, _______},
