d6aa39507220fba83155d7047bfa35635eca4529
Jason Stillwell <dragonfax@gmail.com>
2018-03-16 16:04:03 -0700

    better activation for swap hands

--- users/dragonfax/dragonfax.c
+++ users/dragonfax/dragonfax.c
@@ -35,11 +35,6 @@ enum custom_keycodes {
 #define KC_VOLDN KC__VOLDOWN
 #define NUM_ROW {KC_GRAVE,KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  _______ }
 
-const uint16_t PROGMEM fn_actions[] = {
-  ACTION_SWAP_HANDS_TAP_KEY(KC_SPC),
-};
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* http://www.keyboard-layout-editor.com/#/gists/e0f05dcdbec66445c2f4449b9298b2b1 */
@@ -62,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______},
   {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT },
   {_______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT},
-  {KC_LCTL, _______, KC_LALT, KC_LGUI, LOWER,   KC_FN0,  KC_FN0,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_LCTL, _______, KC_LALT, KC_LGUI, LOWER,   SH_T(KC_SPC),  SH_T(KC_SPC),  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Lower
