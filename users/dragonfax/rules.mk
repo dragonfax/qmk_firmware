b7913f6ab4a60bf3904917cb09b5971d479d4811
Jason Stillwell <dragonfax@gmail.com>
2018-03-21 11:33:43 -0700

    remove swap hands. still causes some typoes

--- users/dragonfax/rules.mk
+++ users/dragonfax/rules.mk
@@ -1,13 +1,12 @@
 
 NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 TAP_DANCE_ENABLE = no
-ONEHAND_ENABLE = no
 EXTRAKEY_ENABLE = no       # Audio control and System control(+450)
 CONSOLE_ENABLE = no         # Console for debug(+400)
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 AUTO_SHIFT_ENABLE = yes
 AUTO_SHIFT_MODIFIERS = yes
-SWAP_HANDS_ENABLE = yes
+SWAP_HANDS_ENABLE = no
 BOOTMAGIC_ENABLE = no
 COMMAND_ENABLE = no
 
